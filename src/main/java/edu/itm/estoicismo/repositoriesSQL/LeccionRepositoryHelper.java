5c7f631dc4e4f6ccd1bd57c56942e942c348af1f
Juan Pablo Deossa Yepes <jdeossa@SFOURGEN.COM>
2026-05-09 14:19:06 -0500

    Refactor proyecto estoicismo y salud mental

---
+++ src/main/java/edu/itm/estoicismo/repositoriesSQL/LeccionRepositoryHelper.java
@@ -0,0 +1,31 @@
+package edu.itm.estoicismo.repositoriesSQL;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LeccionRepositoryHelper {
+
+    public String listarLecciones() {
+        return "SELECT id_leccion, id_ruta, titulo, contenido_teorico, orden_leccion FROM lecciones";
+    }
+
+    public String buscarLeccionPorId() {
+        return "SELECT id_leccion, id_ruta, titulo, contenido_teorico, orden_leccion FROM lecciones WHERE id_leccion = ?";
+    }
+
+    public String obtenerUltimoIdLeccion() {
+        return "SELECT MAX(id_leccion) FROM lecciones";
+    }
+
+    public String insertarLeccion() {
+        return "INSERT INTO lecciones (id_leccion, id_ruta, titulo, contenido_teorico, orden_leccion) VALUES (?, ?, ?, ?, ?)";
+    }
+
+    public String actualizarLeccion() {
+        return "UPDATE lecciones SET id_ruta = ?, titulo = ?, contenido_teorico = ?, orden_leccion = ? WHERE id_leccion = ?";
+    }
+
+    public String eliminarLeccion() {
+        return "DELETE FROM lecciones WHERE id_leccion = ?";
+    }
+}
